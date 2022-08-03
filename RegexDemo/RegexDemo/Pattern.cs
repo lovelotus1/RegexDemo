cbb34333ffe926739884da2f0d51b211f147887d
lovelotus <opera.prem@gmail.com>
2022-08-03 18:59:41 +0530

    Added: Enter a valid Last Name UC2

--- RegexDemo/RegexDemo/Pattern.cs
+++ RegexDemo/RegexDemo/Pattern.cs
@@ -9,12 +9,36 @@ namespace RegexDemo
 {
     public class Pattern
     {
-        public static string FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";//Minimum 3 Char And 1ST Char Must Be UpperCase
-
-        //Method for Validate First Name.
-        public bool ValidateFirstName(string Firstname)
+        //Method to check and take the first name using regex(UC1)
+        public void CheckFirstName()
         {
-            return Regex.IsMatch(Firstname, FIRST_NAME_REGEX);
+
+            string firstName = Console.ReadLine();
+            //Pattern for checking the first name that starts with cap and has min 3 characters(UC1)
+            string FirstNamePattern = "(^[A-Z]{1}[a-z]{2,}$)";
+            if (Regex.IsMatch(firstName, FirstNamePattern))
+            {
+                Console.WriteLine("\tThe entered first name is valid");
+            }
+            else
+            {
+                Console.WriteLine("\tFirst name should starts with Cap and should have minimum 3 characters");
+            }
+        }
+        public void CheckLastName()
+        {
+            string LastName = Console.ReadLine();
+            //Pattern for checking the last name that starts with cap and has min 3 characters(UC2)
+            string LastNamePattern = "(^[A-Z]{1}[a-z]{2,}$)";
+            if (Regex.IsMatch(LastName, LastNamePattern))
+            {
+                Console.WriteLine("\tThe given last name is valid");
+            }
+            else
+            {
+                Console.WriteLine("\tLast name should starts with UpperCase and should have minimum 3 characters");
+            }
+
         }
     }
 }
--- RegexDemo/RegexDemo/Program.cs
+++ RegexDemo/RegexDemo/Program.cs
@@ -9,19 +9,30 @@ namespace RegexDemo
             Console.WriteLine("Welcome to User Registation Problems");
             Pattern program = new Pattern();
             Console.WriteLine("-->Select to Program is Executed<--");
-            Console.WriteLine("1.Enter First Name : \n Exit");
+            Console.WriteLine("1.Enter First Name \n2.Enter Last Name \n3.Exit");
             Console.WriteLine("Select Valid Option:->");
             int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
-            {
-                case 1:
-                    Console.WriteLine(program.ValidateFirstName(instr));
-                    break;
-                case 2:
-                default:
-                    Console.WriteLine("Please Select Correct Option");
-                    break;
-            }
+            bool flag = true;
+            while (flag)
+                switch (option)
+                {
+                    case 1:
+                        //Validating user entered a valid firstname or not(UC1)
+                        Console.Write("\tEnter Your First Name: ");
+                        program.CheckFirstName();//Call the Method.
+                        break;
+                    case 2:
+                        //Validating user entered a valid Lastsname or not(UC2)
+                        Console.Write("\tEnter Your Last Name: ");
+                        program.CheckLastName();//Call the Method.
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("\tSelect Correct Number");
+                        break;
+                }
         }
     }
 }
