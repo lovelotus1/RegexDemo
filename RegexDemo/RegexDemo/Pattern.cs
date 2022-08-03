5d444ac6fe88a14bfeb68848d88ba9ef7ca84ddf
lovelotus <opera.prem@gmail.com>
2022-08-03 18:39:53 +0530

    Added: Enter a valid First Name UC1

---
+++ RegexDemo/RegexDemo/Pattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class Pattern
+    {
+        public static string FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";//Minimum 3 Char And 1ST Char Must Be UpperCase
+
+        //Method for Validate First Name.
+        public bool ValidateFirstName(string Firstname)
+        {
+            return Regex.IsMatch(Firstname, FIRST_NAME_REGEX);
+        }
+    }
+}
--- RegexDemo/RegexDemo/Program.cs
+++ RegexDemo/RegexDemo/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to User Registation Problem!");
+﻿using System;
+namespace RegexDemo
+{
+    class program
+    {
+        public static string instr = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registation Problems");
+            Pattern program = new Pattern();
+            Console.WriteLine("-->Select to Program is Executed<--");
+            Console.WriteLine("1.Enter First Name : \n Exit");
+            Console.WriteLine("Select Valid Option:->");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine(program.ValidateFirstName(instr));
+                    break;
+                case 2:
+                default:
+                    Console.WriteLine("Please Select Correct Option");
+                    break;
+            }
+        }
+    }
+}
