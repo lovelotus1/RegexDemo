1648e24a47088a2c5828542eba5b320b70510b67
lovelotus <opera.prem@gmail.com>
2022-08-03 20:26:15 +0530

    Added: The code to check the User Password  Format Rule4 UC8

--- RegexDemo/RegexDemo/Pattern.cs
+++ RegexDemo/RegexDemo/Pattern.cs
@@ -99,8 +99,12 @@ namespace RegexDemo
         public void CheckPasswrd2()
         {
             string passwrd = Console.ReadLine();
-            string passwrdPattern = "^(?=.*[A-Z])(?=.*[0-9])[0-9a-zA-Z]{8,}$";//Pattern for checking the Passwed Should have at Least 1 Numric Number
-            if (Regex.IsMatch(passwrd, passwrdPattern))
+            //Pattern for checking the Passwed for having atleast (UC5 UC6 UC7 )
+            string passwrdPattern = "^(?=.*[A-Z])(?=.*[\\d])(?=.*[\\W])[a-zA-Z0-9[~!@#$%^&*()_+{}:\"<>?]{8,}$";
+            string spacialChar = "[~!@#$%^&*()_+{}:\" <>?]";
+            int count = Regex.Matches(passwrd, spacialChar).Count();
+            Console.WriteLine(count);
+            if (Regex.IsMatch(passwrd, passwrdPattern) && count == 1) 
             {
                 Console.WriteLine("\t Passwrd is Valid");
             }
